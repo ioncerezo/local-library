c316ca83d9b3a63f3c2e051650deaf1cbb1b7f01
Ion Cerezo <ioncerezovalero@gmail.com>
2023-02-27 15:07:44 +0000

    commit

--- app.js
+++ app.js
@@ -4,8 +4,12 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const mongoose = require("mongoose");
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+
+//routes 
+let indexRouter = require("./routes/index");
+const catalogRouter = require("./routes/catalog")
+
+
 mongoose.set('strictQuery', false);
 var app = express();
 
@@ -30,10 +34,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
+app.use("/", indexRouter);
+app.use("/catalog", catalogRouter);
 app.use(function(req, res, next) {
   next(createError(404));
 });
